a53898fb65514387318286d813e99102cde22129
Ravicha <rsuksawasdi@gmail.com>
2026-03-05 15:30:01 +1100

    Update Vite base path for deployed site

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/for_mena/',
+    base: '/bd_mena/',
     plugins: [react()],
 });
